730fc889eff0fad7077644394ba5312cda2cbcef
alekstheod <alekstheod@gmail.com>
2010-08-27 20:25:08 +0000

    Protocol package move in progress.

---
+++ network/org/promasi/network/protocol/request/LeaveGameRequest.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package org.promasi.network.protocol.request;
+
+
+/**
+ * @author m1cRo
+ *
+ */
+public class LeaveGameRequest extends AbstractRequest
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
