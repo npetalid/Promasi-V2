d75a36f899862a2af4618d2a1684e2f373dd1d85
alekstheod <alekstheod@gmail.com>
2012-01-21 12:03:55 +0100

    Added needed files.

---
+++ org.promasi.client_swing/src/org/promasi/client_swing/gui/desktop/ClockJPanel.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package org.promasi.client_swing.gui.desktop;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.joda.time.DateTime;
+
+/**
+ * @author alekstheod
+ *
+ */
+public class ClockJPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 
+	 */
+	public static final int CONST_CLOCK_PANEL_WIDTH = 120;
+	
+	/**
+	 * 
+	 */
+	private JLabel _clockLabel;
+
+	/**
+	 * 
+	 */
+	public ClockJPanel(){
+		super();
+		setBackground( Color.gray );
+		setPreferredSize( new Dimension(CONST_CLOCK_PANEL_WIDTH, 100) );
+		setLayout(new BorderLayout());
+		_clockLabel = new JLabel();
+		add(_clockLabel, BorderLayout.CENTER);
+	}
+	
+	/**
+	 * 
+	 * @param date
+	 */
+	public void updateTime(DateTime dateTime){
+		if( dateTime != null ){
+			Calendar calendar = new GregorianCalendar();
+			calendar.setTime(dateTime.toDate());
+			SimpleDateFormat dateFromat = new SimpleDateFormat("dd MMMMM kk");
+			_clockLabel.setText( dateFromat.format(calendar.getTime() ) );
+		}
+	}
+}
---
+++ org.promasi.client_swing/src/org/promasi/client_swing/gui/desktop/QuickIconsJPanel.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.promasi.client_swing.gui.desktop;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+/**
+ * @author alekstheod
+ *
+ */
+public class QuickIconsJPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static final int CONST_QUICK_ICONS_PANEL_WIDTH = 200;
+	
+	public QuickIconsJPanel(){
+		super();
+		setBackground(Color.gray);
+		setPreferredSize(new Dimension(CONST_QUICK_ICONS_PANEL_WIDTH,100));
+	}
+
+}
