43f6d4b6e7b1b8b7b041dc2c339ff3eb26473cbf
alekstheod <alekstheod@gmail.com>
2012-02-22 20:40:02 +0100

    the same

--- org.promasi.game/src/org/promasi/game/company/ICompanyListener.java
+++ org.promasi.game/src/org/promasi/game/company/ICompanyListener.java
@@ -8,36 +8,62 @@ import org.promasi.game.project.ProjectMemento;
 
 /**
  * @author m1cRo
- *
+ * Represent an interface of
+ * an Company listener. Listener should be informed
+ * for any major company state change.
  */
 public interface ICompanyListener {
 	/**
-	 * 
-	 * @param project
+	 * Will be called in case if a new project was assigned to 
+	 * the company.
+	 * @param owner Company owner id.
+	 * @param company instance of {@link = CompanyMemento} which
+	 * represent the company.
+	 * @param assignedProject instance of {@link = ProjectMemento} which
+	 * represent the assigned project.
+	 * @param dateTime the current DateTime.
 	 */
 	public void projectAssigned( String owner, CompanyMemento company, ProjectMemento project, DateTime dateTime );
 	
 	/**
-	 * 
-	 * @param project
+	 * Will be called by in case if project finished.
+	 * @param owner Company owner id.
+	 * @param company instance of {@link = CompanyMemento} which
+	 * represent the company.
+	 * @param assignedProject instance of {@link = ProjectMemento} which
+	 * represent the assigned project.
+	 * @param dateTime the current DateTime.
 	 */
 	public void projectFinished( String owner, CompanyMemento company, ProjectMemento project, DateTime dateTime);
 	
 	/**
-	 * 
+	 * Will be called by in case if the company
+	 * is insolvent.
+	 * @param owner Company owner id.
+	 * @param company instance of {@link = CompanyMemento} which
+	 * represent the company.
+	 * @param assignedProject instance of {@link = ProjectMemento} which
+	 * represent the assigned project.
+	 * @param dateTime the current DateTime.
 	 */
 	public void companyIsInsolvent( String owner, CompanyMemento company, ProjectMemento assignedProject, DateTime dateTime);
 
 	/**
-	 * 
-	 * @param company
-	 * @param assignedProject
+	 * Will be called by in case of execution working step.
+	 * @param owner Company owner id.
+	 * @param company instance of {@link = CompanyMemento} which
+	 * represent the company.
+	 * @param assignedProject instance of {@link = ProjectMemento} which
+	 * represent the assigned project.
+	 * @param dateTime the current DateTime.
 	 */
 	public void onExecuteWorkingStep( String owner, CompanyMemento company, ProjectMemento assignedProject, DateTime dateTime);
 	
 	/**
-	 * 
-	 * @param company
+	 * Will inform the listener about his registration.
+	 * @param owner the company owner Id.
+	 * @param company instance of {@link = CompanyMemento} which
+	 * represent the company.
 	 */
 	public void companyAssigned ( String owner, CompanyMemento company );
 }
