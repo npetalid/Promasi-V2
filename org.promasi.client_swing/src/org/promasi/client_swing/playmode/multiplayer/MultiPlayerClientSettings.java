a432cc026538e6f5bf34ff803434117b69911916
alekstheod <alekstheod@gmail.com>
2012-02-23 22:40:08 +0100

    Added lib.

---
+++ org.promasi.client_swing/src/org/promasi/client_swing/playmode/multiplayer/MultiPlayerClientSettings.java
@@ -0,0 +1,55 @@
+package org.promasi.client_swing.playmode.multiplayer;
+
+import org.promasi.utilities.serialization.SerializableObject;
+
+public class MultiPlayerClientSettings extends SerializableObject
+{
+	/**
+	 * 
+	 */
+	private String _hostName;
+	
+	/**
+	 * 
+	 */
+	private int _portNumber;
+	
+	/**
+	 * 
+	 */
+	public MultiPlayerClientSettings(){
+	}
+	
+	/**
+	 * 
+	 * @param _hostName
+	 */
+	public void setHostName(String _hostName) {
+		this._hostName = _hostName;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getHostName() {
+		return _hostName;
+	}
+
+	/**
+	 * 
+	 * @param _portNumber
+	 */
+	public void setPortNumber(int _portNumber) {
+		this._portNumber = _portNumber;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public int getPortNumber() {
+		return _portNumber;
+	}
+	
+}
