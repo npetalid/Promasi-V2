894d9ca59fbb95cf828f49a57ccee3a6aed70726
alekstheod <alekstheod@gmail.com>
2012-04-09 14:07:02 +0200

    Before removing multiplayer.

--- org.promasi.client_swing/src/org/promasi/client_swing/Application.java
+++ org.promasi.client_swing/src/org/promasi/client_swing/Application.java
@@ -36,36 +36,14 @@ public class Application {
 				try {
 					
 					try{
-			            // set your theme
-						//UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
-					    /*for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-					        if ("Nimbus".equals(info.getName())) {
-					            UIManager.setLookAndFeel(info.getClassName());
-					            break;
-					        }
-					    }*/
-			            
 			            UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
 					} catch (Exception e) {
-					    // If Nimbus is not available, you can set the GUI to another look and feel.
+						_logger.warn("Theme applying failed");
 					}
-					
-					//GameMaker.makeGame();
-	                // Create a JFrame, which is a Window with "decorations", i.e.
-	                // title, border and close-button
+
 	                MainFrame mainFrame = new MainFrame("ProMaSi");
-	                
-	                // Set a simple Layout Manager that arranges the contained
-	                // Components
 	                mainFrame.setLayout(new FlowLayout());
-	 
-	                // "Pack" the window, making it "just big enough".
 	                mainFrame.pack();
-	                
-	                // Set the default close operation for the window, or else the
-	                // program won't exit when clicking close button
-	                //  (The default is HIDE_ON_CLOSE, which just makes the window
-	                //  invisible, and thus doesn't exit the app)
 	                mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	                mainFrame.enableWizardMode();
 	                mainFrame.setLayout(new BorderLayout());
@@ -73,11 +51,8 @@ public class Application {
 	        		JPanel panel;
 					panel = new PlayModesJPanel( mainFrame );
 	        		mainFrame.changePanel(panel);
-	                
-	                // Set the visibility as true, thereby displaying it
 	                mainFrame.setVisible(true);
 				} catch (GuiException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
             }
